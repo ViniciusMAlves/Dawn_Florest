80f6cf521492d4a58988958f8cfbb16b46a50498
Vinicius Moreira Alves <37082519+ViniciusMAlves@users.noreply.github.com>
2024-05-28 21:25:50 -0300

    novas movimentações
    adicionado novas movimentação como:
    ataque, bloquear e agachar

--- Dawn Florest/scenes/Player/player_texture.gd
+++ Dawn Florest/scenes/Player/player_texture.gd
@@ -1,13 +1,20 @@
 extends Sprite2D
 class_name PlayerTexture
 
+var normal_attack: bool = false
+var shield_off: bool = false
+var chouching_off: bool = false
+var suffix: String = "_right"
+
 @export var animation_path: AnimationPlayer
 @export var player: CharacterBody2D
 
 func animate(direction: Vector2) -> void:
 	verify_position(direction)
 	
-	if direction.y != 0:
+	if player.attacking or player.defending or player.crouching:
+		action_behavior()
+	elif direction.y != 0:
 		vertical_behavior(direction)
 	elif player.landing:
 		animation_path.play("landing")
@@ -19,8 +26,20 @@ func animate(direction: Vector2) -> void:
 func verify_position(direction: Vector2)-> void:
 	if direction.x > 0:
 		flip_h = false
+		suffix = "_right"
 	elif direction.x < 0:
 		flip_h = true
+		suffix = "_left"
+
+func action_behavior() -> void:
+	if player.attacking and normal_attack:
+		animation_path.play("attack" + suffix)
+	elif  player.defending and shield_off:
+		animation_path.play("shield")
+		shield_off = false
+	elif player.crouching and chouching_off:
+		animation_path.play("crouch")
+		chouching_off = false
 
 func vertical_behavior(direction: Vector2)-> void:
 	if direction.y > 0:
@@ -41,3 +60,11 @@ func _on_animation_finished(anim_name):
 		"landing":
 			player.landing = false
 			player.set_physics_process(true)
+			
+		"attack_left":
+			normal_attack = false
+			player.attacking = false
+			
+		"attack_right":
+			normal_attack = false
+			player.attacking = false
--- Dawn Florest/scripts/player/player.gd
+++ Dawn Florest/scripts/player/player.gd
@@ -9,7 +9,13 @@ class_name Player
 @export var player_gravity = 100
 
 var jump_count:int = 0
-var landing:bool = false
+
+var landing: bool = false
+var attacking: bool = false
+var defending: bool = false
+var crouching: bool = false
+
+var can_track_input: bool = true
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -19,6 +25,7 @@ func _physics_process(delta):
 	horizontal_movememt_env()
 	vertical_movement_env()
 	gravidade(delta)
+	action_env()
 	
 	move_and_slide()
 	player_sprite.animate(velocity)
@@ -26,6 +33,10 @@ func _physics_process(delta):
 
 func  horizontal_movememt_env() -> void:
 	var direction = Input.get_axis("ui_left", "ui_right")
+	if (not can_track_input) or attacking:
+		velocity.x = 0
+		return
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
@@ -35,10 +46,39 @@ func vertical_movement_env() -> void:
 	if is_on_floor():
 		jump_count = 0
 		
-	if Input.is_action_just_pressed("ui_select") and jump_count < 2:
+	if Input.is_action_just_pressed("jump") and jump_count < 2 and can_track_input and (not attacking):
 		jump_count += 1
 		velocity.y = JUMP_VELOCITY
-	
+
+func action_env() -> void:
+	attack()
+	crounch()
+	defense()
+
+func attack() -> void:
+	var attack_condition = (not attacking) and (not crouching) and (not defending)
+	if Input.is_action_just_pressed("attack") and attack_condition and is_on_floor():
+		attacking = true
+		player_sprite.normal_attack = true
+
+func crounch() -> void:
+	if Input.is_action_pressed("crouch") and is_on_floor() and not defending:
+		crouching = true
+		can_track_input = false
+	elif not defending:
+		crouching = false
+		can_track_input = true
+		player_sprite.chouching_off = true
+
+func defense() -> void:
+	if Input.is_action_pressed("defense") and is_on_floor() and not crouching:
+		defending = true
+		can_track_input = false
+	elif not crouching:
+		defending = false
+		can_track_input = true
+		player_sprite.shield_off = true
+
 func gravidade(delta: float) -> void:
 	velocity.y += delta * player_gravity
 	if velocity.y >= player_gravity:
